11c3c71d8c88f9ce485c979bf4025c554622beb3
Ebberth <ebberth@gmal.com>
2016-01-27 21:47:24 -0200

    DataProduct

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(markdown)
 
 shinyServer(function(input, output, session) {
 
--- ui.R
+++ ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(markdown)
 
 # Define UI for application that plots random distributions 
 shinyUI(pageWithSidebar(
@@ -31,10 +32,20 @@ shinyUI(pageWithSidebar(
       
       tabPanel(p(icon("link"), "Links"), value = '3',
                h2("Links"),
-               p(strong(em("Documentation:",a("Online Conversion - Temperature Conversion",href="READMe.html")))),
-               p(strong(em("Github repository:",
+               p(strong(em("Documentation:",a("Online Conversion - Temperature Conversion",href="README.html")))),
+               p(strong(em("Github repository:", 
                            a("Developing Data Products - Peer Assessment Project; Shiny App",
-                             href="https://github.com/Ebberth/DevelopingDataProducts"))))
+                           href="https://github.com/Ebberth/DataProduct")))
+               ),
+               h2("Interesting Links"),
+               p(strong(em("Wikipedia:",a("Kelvin Scale",href="https://en.wikipedia.org/wiki/Kelvin")))),
+               p(strong(em("Wikipedia:",a("Celsius Scale",href="https://en.wikipedia.org/wiki/Celsius")))),
+               p(strong(em("Wikipedia:",a("Fahrenheit Scale",href="https://en.wikipedia.org/wiki/Fahrenheit")))),
+               p(strong(em("Wikipedia:",a("Scale of temperature",href="https://en.wikipedia.org/wiki/Scale_of_temperature")))),
+               p(strong(em("Wikipedia:",a("Temperature",href="https://en.wikipedia.org/wiki/Temperature")))),
+               p(strong(em("Wikipedia:",a("Conversion of units of temperature",href="https://en.wikipedia.org/wiki/Conversion_of_units_of_temperature")))),
+               p(strong(em("Allmeasures:",a("Temperature Conversion Table",href="http://www.allmeasures.com/temperature.html")))),
+               p(strong(em("Atmos:",a("From fahrenheit to celsius to kelvin and back",href="http://ww2010.atmos.uiuc.edu/(Gh)/guides/maps/ctof.rxml"))))
       )
     )
   )
